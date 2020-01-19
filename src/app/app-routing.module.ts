271fe49a06a3a6a4eccbacd7e715819704e49d9b
Latte <stardustdev@outlook.com>
2020-01-19 15:20:12 +1000

    Set format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GoogleComponent } from "./google/google.component";
+import { BingComponent } from "./bing/bing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "google", component: GoogleComponent },
+  { path: "bing", component: BingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/bing/bing.component.ts
+++ src/app/bing/bing.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bing.component.scss']
 })
 export class BingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
